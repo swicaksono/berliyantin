7cd3b66d01119d37a4679a73ad132d0214ec57c9
swicaksono <swicaksono@admins-MacBook-Pro-4.local>
2022-08-12 19:51:49 +0700

    Update project name and settings

--- site.config.ts
+++ site.config.ts
@@ -2,24 +2,24 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: '9f7c3240c5ec48759d7d9b90ae159962',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'Berliyantin personal blogs',
+  domain: 'berliyantin.com',
+  author: 'Berliyantin Puspaningrum',
 
   // open graph metadata (optional)
-  description: 'Example Next.js Notion Starter Kit Site',
+  description: 'I am Berliyantin Puspaningrum, an amateur media geek and a part-timer pop-culture enthusiast! I wish you, whoever you are reading this, a perfect day.',
 
   // social usernames (optional)
-  twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
-  linkedin: 'fisch2',
+  twitter: 'berliyantinp',
+  github: 'berliyantinp',
+  linkedin: 'berliyantinp',
   // newsletter: '#', // optional newsletter URL
   // youtube: '#', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
 
